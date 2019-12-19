fc7b495e654f2e2ed6abd4b19a4f981744a69b1d
Zhunisali <zshanabek@gmail.com>
2019-12-19 18:48:44 +0600

    city

--- house/views.py
+++ house/views.py
@@ -16,8 +16,8 @@ class HouseViewSet(ModelViewSet):
     queryset = house_models.House.objects.all()
     serializer_class = home_serializers.HouseSerializer
     filter_backends = [DjangoFilterBackend]
-    search_fields = ('address', 'city__name')
-    filter_fields = ['floor', 'rooms', 'beds', 'price', 'house_type', 'rating']
+    search_fields = ('address',)
+    filter_fields = ['floor', 'rooms', 'beds', 'price', 'house_type', 'rating', 'city']
     ordering_fields = ['rating', 'price']
 
     def get_queryset(self):
