a642e817a74bb835d5c3e63e9545fb11c10c18e7
Zhunisali <zshanabek@gmail.com>
2020-01-06 19:38:48 +0600

    ok

--- akv/settings.py
+++ akv/settings.py
@@ -65,13 +65,12 @@ NOTIFICATIONS_CHANNELS = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'akv.urls'
