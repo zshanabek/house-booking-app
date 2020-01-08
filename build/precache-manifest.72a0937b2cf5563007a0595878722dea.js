e8a2baf2407a509ef48400f33290bb1951b5639d
Zhunisali <zshanabek@gmail.com>
2020-01-08 19:12:27 +0600

    with build react

---
+++ build/precache-manifest.72a0937b2cf5563007a0595878722dea.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e70398bce513f2e4a092ed5bae5e381",
+    "url": "/akv/index.html"
+  },
+  {
+    "revision": "d0caea2b925aa9914c09",
+    "url": "/akv/static/css/2.bea88d9d.chunk.css"
+  },
+  {
+    "revision": "36fd0d52b13c2e829e62",
+    "url": "/akv/static/css/main.5282de50.chunk.css"
+  },
+  {
+    "revision": "d0caea2b925aa9914c09",
+    "url": "/akv/static/js/2.9023370e.chunk.js"
+  },
+  {
+    "revision": "36fd0d52b13c2e829e62",
+    "url": "/akv/static/js/main.7867888a.chunk.js"
+  },
+  {
+    "revision": "5534ad7e284b7b997738",
+    "url": "/akv/static/js/runtime-main.f8ef13dd.js"
+  },
+  {
+    "revision": "4e8821cb727d4a3d1fc5527b6404131a",
+    "url": "/akv/static/media/akv-logo-kz.4e8821cb.png"
+  },
+  {
+    "revision": "2c376ec35cab986de6045a552eff8227",
+    "url": "/akv/static/media/apartments.2c376ec3.png"
+  },
+  {
+    "revision": "ff3df4b1979d4930a207a13999dd9ad3",
+    "url": "/akv/static/media/dacha.ff3df4b1.png"
+  },
+  {
+    "revision": "3e6fe4723c30fb09a4d2e206c8ec0136",
+    "url": "/akv/static/media/eventsImage1.3e6fe472.png"
+  },
+  {
+    "revision": "7146397984676839435bce2af9a764a3",
+    "url": "/akv/static/media/eventsImage2.71463979.png"
+  },
+  {
+    "revision": "510d0b2472cb42f8f0661c777ca5bd6c",
+    "url": "/akv/static/media/eventsImage3.510d0b24.png"
+  },
+  {
+    "revision": "f8e59f03a51ae1e1e62935f97ffdb24b",
+    "url": "/akv/static/media/eventsImage4.f8e59f03.png"
+  },
+  {
+    "revision": "84d4a861131d1bf542a64bc1df916854",
+    "url": "/akv/static/media/eventsImage5.84d4a861.png"
+  },
+  {
+    "revision": "55d342acd3ac56f30c5061982ea0c78a",
+    "url": "/akv/static/media/eventsImage6.55d342ac.png"
+  },
+  {
+    "revision": "03d3cba5ba5271f48c50d279f118c54a",
+    "url": "/akv/static/media/feedback-bg.03d3cba5.png"
+  },
+  {
+    "revision": "af222e29e829868ad1de31db5972014b",
+    "url": "/akv/static/media/house 1.af222e29.png"
+  },
+  {
+    "revision": "634e695095929061cf5e485dac2f2d98",
+    "url": "/akv/static/media/house.634e6950.png"
+  },
+  {
+    "revision": "cdc8b02d86ce0e9e185cf048e316391d",
+    "url": "/akv/static/media/image 1.cdc8b02d.png"
+  },
+  {
+    "revision": "0c0bfdaf4eb327182f0423973a38f74a",
+    "url": "/akv/static/media/image 2.0c0bfdaf.png"
+  },
+  {
+    "revision": "9665c78b7d2368bc2de452b11b318f51",
+    "url": "/akv/static/media/image 3.9665c78b.png"
+  },
+  {
+    "revision": "d55ec04ee999f821160a5ee8628f2ade",
+    "url": "/akv/static/media/image 4.d55ec04e.png"
+  },
+  {
+    "revision": "89b17d9c4c9ad1d8df9c051f8fe18ecf",
+    "url": "/akv/static/media/main-bg.89b17d9c.png"
+  },
+  {
+    "revision": "7ef2c9eb3e1218a9f1a43c4c507b4dbb",
+    "url": "/akv/static/media/offer-bg.7ef2c9eb.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/akv/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/akv/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/akv/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/akv/static/media/slick.f97e3bbf.svg"
+  }
+]);
