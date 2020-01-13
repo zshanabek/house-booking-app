8a3552cca2fecda75477fc5abcd8bc26f6642e5b
Zhunisali <zshanabek@gmail.com>
2020-01-14 00:37:42 +0600

    daphne

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn akv.wsgi --log-file -
+web2: daphne akv.routing:application --port $PORT --bind 0.0.0.0 -v2 
--- connect.py
+++ connect.py
@@ -22,11 +22,14 @@ def on_open(ws):
     print("### opened ###")
 
 
+token = "64346f05509d5bcc4a0c7672626fd2ee1334593a"
+# token = "ece27aa267d077b65336e90d8b09f376c82741bb"
+host = "localhost:8000"
+# host = "akv-technopark.herokuapp.com:5864211"
+url = "ws://{}/ws?token={}".format(host, token)
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://localhost:8000/ws?token=64346f05509d5bcc4a0c7672626fd2ee1334593a",
-    # ws = websocket.WebSocketApp("ws://akv-technopark.herokuapp.com:44810/3ca5b05abc2c463",
-    # ws = websocket.WebSocketApp("ws://localhost:8081/0b1d940686904e2",
+    ws = websocket.WebSocketApp(url,
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
