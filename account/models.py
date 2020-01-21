38bbed84c6f00a096468710bfe8ca8dc2722b782
Zhunisali <zshanabek@gmail.com>
2020-01-21 23:59:08 +0600

    user_type add

--- account/models.py
+++ account/models.py
@@ -4,6 +4,13 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from .managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
 
+GUEST = 0
+HOST = 1
+USER_TYPE_CHOICES = (
+    (GUEST, 'Guest'),
+    (HOST, 'Host'),
+)
+
 
 class User(AbstractBaseUser):
     email = models.EmailField(verbose_name='email', max_length=60, unique=True)
@@ -13,6 +20,7 @@ class User(AbstractBaseUser):
         validators=[MinValueValidator(0), MaxValueValidator(1)])
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    user_type = models.IntegerField(default=0, choices=USER_TYPE_CHOICES)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -22,13 +30,13 @@ class User(AbstractBaseUser):
     objects = UserManager()
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['phone', 'birth_day',
-                       'gender', 'first_name', 'last_name']
-    FIELDS_TO_UPDATE = ['birth_day', 'email',
-                        'gender', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ('phone', 'birth_day', 'gender',
+                       'first_name', 'last_name')
+    FIELDS_TO_UPDATE = ('birth_day', 'email', 'gender',
+                        'first_name', 'last_name', 'user_type')
 
     def __str__(self):
-        return f"{self.email} - {self.first_name} {self.last_name}"
+        return f"{self.id}; {self.email}; {self.first_name} {self.last_name}"
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
--- account/serializers.py
+++ account/serializers.py
@@ -18,7 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'email', 'gender', 'phone',
-                  'first_name', 'last_name', 'birth_day', 'userpic', 'is_active')
+                  'first_name', 'last_name', 'birth_day', 'userpic', 'is_active', 'user_type')
 
 
 class LoginTokenSerializer(TokenSerializer):
