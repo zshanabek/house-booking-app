0b5b1b99748075f60786d6722d628ffdba8aa526
Zhunisali <zshanabek@gmail.com>
2020-01-31 00:48:24 +0600

    add houseshortserializer

--- reservation/serializers.py
+++ reservation/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 from .models import Reservation
-from house.models import House
+from house.models import House, Photo
 from account.serializers import UserShortSerializer
 from account.models import User
 from datetime import datetime
 from .tasks import send_email_task, set_reservation_as_inactive
+
+
+class HouseShortSerializer(serializers.ModelSerializer):
+    photos = serializers.SerializerMethodField()
+
+    def get_photos(self, obj):
+        p_qs = Photo.objects.filter(house=obj)
+        if len(p_qs) == 0:
+            return None
+        photos = PhotoSerializer(p_qs, many=True).data
+        return photos
+    class Meta:
+        model = House
+        fields = ('id', 'name', 'photos')
 
 
 class ReservationDatesSerializer(serializers.ModelSerializer):
@@ -18,12 +32,13 @@ class ReservationDatesSerializer(serializers.ModelSerializer):
 
 class ReservationSerializer(serializers.ModelSerializer):
     user = UserShortSerializer(read_only=True)
+    house = HouseShortSerializer()
     house_id = serializers.PrimaryKeyRelatedField(
-        queryset=House.objects.all(), source='house')
+        queryset=House.objects.all(), source='house', write_only=True)
     owner = UserShortSerializer(read_only=True, source='house.user')
 
     class Meta:
         model = Reservation
         fields = ('id', 'check_in', 'check_out', 'days', 'guests', 'status', 'created_at',
-                  'accepted_house', 'user', 'house_id', 'owner', 'message')
+                  'accepted_house', 'user', 'house', 'house_id', 'owner', 'message')
         read_only_fields = ('message', 'status', 'days',)
