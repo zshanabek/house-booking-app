26cedcd51d0e9f6215416ab17db1c563737f1423
Zhunisali <zshanabek@gmail.com>
2020-02-05 02:19:57 +0600

    permission payment

--- order/views.py
+++ order/views.py
@@ -13,15 +13,19 @@ from rest_framework.response import Response
 def create_payment(request):
     reservation_id = request.data.get('reservation_id')
     reserv = get_object_or_404(Reservation, pk=reservation_id)
-    order = Order.objects.create(amount=reserv.income, reservation=reserv)
-    p = Payment(reserv.income, "KZT", "Оплата за проживание в доме", str(order.id))
-    r = p.create_payment()
-    order.payment_id = r.json()['id']
-    order.save()
-    if r.status_code == status.HTTP_201_CREATED:
-        return Response(r.json(), status.HTTP_200_OK)
+    if reserv.status != 1 and reserv.accepted_house: 
+        order = Order.objects.create(amount=reserv.income, reservation=reserv)
+        p = Payment(reserv.income, "KZT", "Оплата за проживание в доме", str(order.id))
+        r = p.create_payment()
+        order.payment_id = r.json()['id']
+        order.save()
+        if r.status_code == status.HTTP_201_CREATED:
+            return Response(r.json(), status.HTTP_200_OK)
+        else:
+            return Response({'response': False, 'error_message': r.json()}, status.HTTP_500_INTERNAL_SERVER_ERROR)
     else:
-        return Response({'response': False, 'error_message': r.json()}, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'response': False, 'error_message': 'Невозможно оплатить, так как бронь была отменена или не принята хозяином'}, status.HTTP_403_FORBIDDEN)
+
 
 @api_view()
 def get_payment(request, pk):
