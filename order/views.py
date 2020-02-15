2d642e9583a1cb3adb4b37eb62dbcb0fb066d228
Zhunisali <zshanabek@gmail.com>
2020-02-16 03:35:49 +0600

    fix permissions import

--- order/views.py
+++ order/views.py
@@ -1,12 +1,13 @@
 
 from rest_framework.decorators import api_view
-from rest_framework import request, status, viewsets
+from rest_framework import request, status, viewsets, permissions
 from .models import Order
 from django.shortcuts import get_object_or_404
 from reservation.models import Reservation
 from utils.payment import Payment, get_payment_details
 from rest_framework.response import Response
 from .serializers import OrderSerializer
+from house.permissions import IsOwnerOrReadOnly
 
 
 @api_view(['POST'])
