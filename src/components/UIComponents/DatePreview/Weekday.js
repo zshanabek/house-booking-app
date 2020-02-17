7c668de1a3cb16d3a5836cc125e25bd868a59a9e
Zhunisali <zshanabek@gmail.com>
2020-02-17 09:26:53 +0600

    clear repo

--- src/components/UIComponents/DatePreview/Weekday.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Weekday({ label, title, current }) {
-  return (
-    <div aria-label={label} className="Weekday">
-      {title}
-    </div>
-  );
-}
