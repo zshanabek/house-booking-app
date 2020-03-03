a58d2cf4093211f997c35c6bd72fbe8f8c5847b6
Zhunisali <zshanabek@gmail.com>
2020-03-03 23:01:13 +0600

    add user to payments

--- order/serializers.py
+++ order/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from .models import Order
 from reservation.models import Reservation
+from account.serializers import UserShortSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
     reservation_id = serializers.PrimaryKeyRelatedField(
         queryset=Reservation.objects.all(), source='reservation')
+    user = UserShortSerializer(read_only=True, source='reservation.house.user')
 
     class Meta:
         model = Order
-        fields = ('id', 'amount', 'is_paid', 'payment_id', 'reservation_id')
-        read_only_fields = ('id', 'amount', 'is_paid', 'payment_id')
+        fields = ('id', 'amount', 'is_paid',
+                  'payment_id', 'reservation_id', 'user')
+        read_only_fields = ('id', 'amount', 'is_paid', 'payment_id', 'user')
